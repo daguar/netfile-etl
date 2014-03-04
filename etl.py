a1d29d256e0ede4012e01ba58a74637e677d8c70
Dave Guarino <dave@codeforamerica.org>
2014-03-04 15:25:55 -0800

    Minor tweaks to etl.py

--- etl.py
+++ etl.py
@@ -3,12 +3,13 @@ import os
 import glob
 
 xlsx_files_in_current_dir = glob.glob("*.xlsx")
+
 for excel_filename in xlsx_files_in_current_dir:
   print "Opening " + excel_filename + "..."
   workbook = xlrd.open_workbook("./" + excel_filename)
   filename_without_extension = excel_filename.split(".")[0]
-  print filename_without_extension
   sheet_names = workbook.sheet_names()
   for sheet_name in sheet_names:
-    print "Writing " + sheet_name + ".csv..."
-    os.system("in2csv " + filename_without_extension + " --sheet " + "\"" + sheet_name + "\"" + " > " + "csv_files/" + sheet_name + ".csv")
+    csv_filename = filename_without_extension + "_" +sheet_name + ".csv"
+    print "Writing " + csv_filename
+    os.system("in2csv " + excel_filename + " --sheet " + "\"" + sheet_name + "\"" + " > " + csv_filename)
